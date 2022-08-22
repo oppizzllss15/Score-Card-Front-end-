2a02cd7a007c365688e47c723e3fab0038d8fd49
Adegoke Surajudeen <surajudeenadegoke@gmail.com>
2022-08-22 23:01:51 +0100

    admin

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,24 @@ import './App.css';
 import {Login} from './pages/Login/Login';
 import { Signup } from "./pages/SignUp/Signup";
 import { DashboardLayout } from './pages/DashboardLayout/DashboardLayout';
+import { Create_admin } from "./pages/Create-admin/Create_admin";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Forget />} />
-      <Route path="/reset-password/:id/:token" element={<Reset />} />
-      <Route path="/dashboard" element={<DashboardLayout/>}>
-      </Route>
-      <Route path="/update-password" element={<Change/>} />
-      <Route path="/login" element={<Login/>} />
-      <Route path="/signup" element={<Signup />} />
-    </Routes>
+    // <Routes>
+    //   <Route path="/" element={<Forget />} />
+    //   <Route path="/reset-password/:id/:token" element={<Reset />} />
+    //   <Route path="/dashboard" element={<DashboardLayout/>}>
+    //   </Route>
+    //   <Route path="/update-password" element={<Change/>} />
+    //   <Route path="/login" element={<Login/>} />
+    //   <Route path="/signup" element={<Signup />} />
+    // </Routes>
+
+<div>
+<Create_admin />
+</div>
+
   )
 }
 
