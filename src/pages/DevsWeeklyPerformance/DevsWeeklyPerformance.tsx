060c846f580542e50ce794f715825515e813fc8b
Opeyemi Ogungbade <Opeyemi.Ogungbade@gmail.com>
2022-08-22 06:27:14 +0100

    devs performance first commit

---
+++ src/pages/DevsWeeklyPerformance/DevsWeeklyPerformance.tsx
@@ -0,0 +1,22 @@
+import { DashboardLayout } from "../DashboardLayout/DashboardLayout";
+import "../DashboardLayout/DashboardLayout.css";
+
+const DevsWeeklyPerformance = () => {
+  return (
+    <div>
+      <div>
+        <DashboardLayout />
+      </div>
+      <div className="sidebar">
+      <div className="dashboard">
+    <img src="" alt="dashboard" />
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default DevsWeeklyPerformance;
