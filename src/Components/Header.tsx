fad200f9559c44556d668e7bfaba9dce625ce60a
azeezcodes <81769914+azeezcodes@users.noreply.github.com>
2022-08-26 15:46:23 +0100

    dashboard done

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,26 +1,42 @@
-import { Logo } from './Logo';
-import { FaSearch } from 'react-icons/fa';
+import { useState } from "react";
+import { Logo } from "./Logo";
+import { FaSearch } from "react-icons/fa";
+import { ProfileModal } from "./ProfileModal";
+
+
 
 export const Header = () => {
-  return (
-     <div className="navbar">
-        <div className="logo">
-           <Logo />
-        </div>
 
-        <div className="search">
-           <div className="sea">
-              <FaSearch />
-              <input type="text" placeholder="Search" className="seaIn" />
-           </div>
-        </div>
+   const [isActive, setIsActive] = useState(false);
 
-        <div className="profile">
-           <div className="image"></div>
-           <div className="name">
-              <p>User</p>
-           </div>
-        </div>
-     </div>
-  );
-}
+    const getFormModal = () => {
+       if (isActive) setIsActive(false);
+       else setIsActive(true);
+    };
+
+   return (
+      <div className="navbar">
+         <div className="logo">
+            <Logo />
+         </div>
+
+         <div className="search">
+            <div className="sea">
+               <FaSearch />
+               <input type="text" placeholder="Search" className="seaIn" />
+            </div>
+         </div>
+
+         <div onClick={getFormModal} className="profile">
+            <div className="image"></div>
+            <div className="name">
+               <p>User</p>
+            </div>
+            {isActive &&  
+            <ProfileModal />
+            }
+         </div>
+
+      </div>
+   );
+};
--- src/Components/SideBar.tsx
+++ src/Components/SideBar.tsx
@@ -5,32 +5,34 @@ import { AiOutlineUserAdd } from "react-icons/ai";
 import { TbArrowBarRight } from "react-icons/tb";
 
 export const SideBar = () => {
-  return (
-    <div className="sidebar">
-      <div className="content-link">
-        <Link to="/dashboard">
-          <div className="dash_icon">
-            <GrHomeRounded /> <p>Dashboard</p>
-          </div>
-        </Link>
-        <Link to="/createuser">
-          <div className="dash_icon">
-            <BiUser /> <p>User Management</p>
-          </div>
-        </Link>
-        <Link to="/createadmin">
-          <div className="dash_icon">
-            <AiOutlineUserAdd /> <p>Admin Management</p>
-          </div>
-        </Link>
-      </div>
+   return (
+      <div className="sidebar">
+         <div className="content-link">
+            <Link to="/dashboard">
+               <div className="dash_icon" id="1">
+                  <GrHomeRounded /> <p>Dashboard</p>
+               </div>
+            </Link>
+            <Link to="/createuser">
+               <div className="dash_icon" id="2">
+                  <BiUser /> <p>User Management</p>
+               </div>
+            </Link>
+            <Link to="/createadmin">
+               <div className="dash_icon" id="3">
+                  <AiOutlineUserAdd /> <p>Admin Management</p>
+               </div>
+            </Link>
+         </div>
 
-      <div className="bottom">
-        <div className="logout">
-          <TbArrowBarRight />
-          <p>Logout</p>
-        </div>
+         <div className="bottom">
+            <Link to="/login">
+               <div className="logout">
+                  <TbArrowBarRight />
+                  <p>Logout</p>
+               </div>
+            </Link>
+         </div>
       </div>
-    </div>
-  );
+   );
 };
